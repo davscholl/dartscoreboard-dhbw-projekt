64f5136f1e1ecfeb7ebe8e58ef273b2e04dde540
davscholl <51119557+davscholl@users.noreply.github.com>
2019-06-02 04:45:58 +0200

    change module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CreateGameComponent } from './create-game/create-game.component';
 import { GameComponent } from './game/game.component';
+import { JoinGameComponent } from './join-game/join-game.component';
+import { LoginComponent } from './login/login.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CreateGameComponent,
-    GameComponent
+    GameComponent,
+    JoinGameComponent,
+    LoginComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
