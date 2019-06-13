911bf15845c30eec57b41a4897bb90ac7d82ee32
davscholl <51119557+davscholl@users.noreply.github.com>
2019-06-13 20:27:04 +0200

    login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
-  ngOnInit() {
-  }
+
+  login(f: NgForm): void {
+    const param = f.value;
+    this.http.post('/api/login', param)
+      .subscribe(
+        (val) => {
+            console.log('POST call successful value returned in body',
+                        val);
+        },
+        response => {
+
+            console.log('POST call in error', response);
+        },
+        () => {
+            console.log('The POST observable is now completed.');
+        });
+    }
+
 
 }
